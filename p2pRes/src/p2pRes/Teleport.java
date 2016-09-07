7ec3251f32fad0e69e17dc1da9b38e07c2a1e1cd
GuyWong <GuyWong@dtc.com>
2016-09-07 18:02:52 +0100

    rename main

---
+++ p2pRes/src/p2pRes/Teleport.java
@@ -0,0 +1,10 @@
+package p2pRes;
+
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import p2pRes.ui.UIMain;
+
+public class Teleport {
+	public static void main(String[] args) {
+		DesktopApplicationContext.main(UIMain.class, args);
+	}
+}
